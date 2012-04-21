0c8483beaaec71f1e24d46d9baa2f50a0edec864
fakeroot <fakeroot@linux-w1cq.site>
2012-04-21 13:48:29 +0200

    Changes made by SparkleShare

--- nb-town.user.js
+++ nb-town.user.js
@@ -8,12 +8,12 @@
 
 //==Optionen==
 var FIX_CHAT = 1; //1 = Chat wird fixiert; 0 = keine Änderung
-var HIDE_FRIENDS = 0; //1 = versteckt den "Freunde online" Tab. Der wird durch die Chatliste sowieso überflüssig.; 0 = keine Änderung
-var HIDE_ADDS = 0; //1 = versteckt Werbeeinblendungen auf der Startseite und den Userpages; 0 = keine Änderungen
+var HIDE_FRIENDS = 1; //1 = versteckt den "Freunde online" Tab. Der wird durch die Chatliste sowieso überflüssig.; 0 = keine Änderung
+var HIDE_ADDS = 1; //1 = versteckt Werbeeinblendungen auf der Startseite und den Userpages; 0 = keine Änderungen
 //==/Optionen==
 
 
-var chatStyle = ".m_friendlist_start { position:fixed !important; }";
+var chatStyle = ".m_friendlist_start { position:fixed !important; z-index: 100 !important; }";
 var friendsStyle = "#td_friends { display: none !important; }";
 var addsStyle = ".inhalt1 { top: 180px !important; } #google_ads_div_468iger_ad_wrapper { display: none !important; } .werbung1 { display: none; } .buttons1 { display: none; }";
 
